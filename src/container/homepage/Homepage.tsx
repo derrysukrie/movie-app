015f0595524082cb0d9d7bdea2edabac4efbd4c6
Derry <43488373+derrysukrie@users.noreply.github.com>
2021-06-15 16:17:52 +0700

    fix import goddamnit

--- src/container/homepage/Homepage.tsx
+++ src/container/homepage/Homepage.tsx
@@ -1,22 +1,9 @@
 import { Block } from 'components/styled';
-import { useEffect } from 'react';
-// import ShowingNow from './_components/showing-now/ShowingNow';
-import TrendingCategories from './_components/trending-categories/TrendingCategories';
-import TVShows from './_components/tv-shows/TVshows';
 
 function Homepage() {
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1');
-  }, []);
-
   return (
     <Block>
-      {/* <Block>
-        <Header />
-      </Block> */}
-      {/* <ShowingNow /> */}
-      <TrendingCategories />
-      <TVShows />
+      ad
     </Block>
   );
 }
